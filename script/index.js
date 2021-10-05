8f816b6ba46f01acabf9dd0106b1e8b4b715637d
levosay <ryuuzaki44l@gmail.com>
2021-10-05 16:58:52 +0300

    refactoring, added comments

--- script/index.js
+++ script/index.js
@@ -1,66 +1,79 @@
 const app = document.querySelector('#app')
+const linkUsers = 'https://json.medrating.org/users/'
+const linkAlbum = 'https://json.medrating.org/albums?userId='
+const linkPhoto = 'https://json.medrating.org/photos?albumId='
 
-// https://json.medrating.org/users/
-// https://json.medrating.org/albums?userId=3
-// https://json.medrating.org/photos?albumId=2
-
+// Обработчик на создание/удаление списков
 app.addEventListener('click', event => {
   let element = event.target
   let elemAlbum = element.querySelector('.list__album')
   let elemPhoto = element.querySelector('.list__photo')
   let elemClass = element.className
-  let userId = element.getAttribute('data-id')
+  let id = element.getAttribute('data-id')
 
-  if (elemClass === 'list__user' && !elemAlbum)
-    getData
-    (userId, element, 'album', 'https://json.medrating.org/albums?userId=')
-
-  if (elemClass === 'list__album' && !elemPhoto)
-    getData
-    (userId, element, 'photo', 'https://json.medrating.org/photos?albumId=')
-
-  clearAlbums(element)
+//Запрос для альбомов
+  if (elemClass === 'list__user' && !elemAlbum) getData(linkAlbum, id, element)
+//Запрос для фото
+  if (elemClass === 'list__album' && !elemPhoto) getData(linkPhoto, id, element)
+//Удаление
+  clearElem(element)
 })
 
-fetch('https://json.medrating.org/users/')
+//Создание списка пользователей
+let ul = document.createElement('ul')
+ul.classList.add('container', 'list')
+fetch(linkUsers)
   .then((response) => response.json())
   .then((data) => data.forEach( element => {
-    let userList = document.createElement('ul')
     let user = document.createElement('li')
-    userList.classList.add('container', 'list')
-    app.append(userList)
     user.setAttribute('data-id', element.id)
     user.classList.add('list__user')
     user.innerHTML = element.name
-    userList.append(user)
+    ul.append(user)
   }))
+app.append(ul)
 
-const createElem = (inPoint, element, name) => {
-  console.log(element)
-  let ul = document.createElement('ul')
-  let li = document.createElement('li')
-  ul.classList.add('list')
-  inPoint.append(ul)
-  ul.append(li)
-  li.setAttribute('data-id', element.id)
-  li.classList.add(`list__${name}`)
-  element.url
-    ?
-    li.innerHTML = `<img src=${element.url} alt=${element.title} class="list__photo">`
-    :
-    li.innerHTML = element.title
-  ul.append(li)
-}
 
-const clearAlbums = (element) => {
-  let userAlbums = element.querySelectorAll('.list')
-  userAlbums.forEach(elem => elem.remove())
-}
-
-const getData = (id, inPoint, name, url) => {
+// Получение данных по ссылке и отправка их на создание разметки
+const getData = (url, id, inPoint) => {
   fetch(`${url}${id}`)
     .then((response) => response.json())
-    .then((data) => data.forEach( element => createElem(inPoint, element, name)))
+    .then((data) => createElem(data, inPoint))
+}
+
+// Создание елементов и вставка их по inPoint
+const createElem = (data, inPoint) => {
+  let ul = document.createElement('ul')
+  ul.classList.add('list')
+
+  data.forEach(element => {
+    let li = document.createElement('li')
+
+    // Если событие на list__album
+    if (element.url) {
+      let img = document.createElement('img')
+      img.classList.add('photo')
+      img.src = element.url
+      img.alt=element.title
+      img.class='photo'
+      li.classList.add('list__photo')
+      li.append(img)
+      ul.append(li)
+      inPoint.append(ul)
+    } else { // Если событие на list__user
+      li.setAttribute('data-id', element.id)
+      li.classList.add('list__album')
+      li.innerHTML = element.title
+      ul.append(li)
+      inPoint.append(ul)
+    }
+  })
+}
+
+// Удаление елементов
+const clearElem = (element) => {
+  let userAlbums = element.querySelectorAll('.list')
+  userAlbums.forEach(elem => elem.remove())
 }
 
 
